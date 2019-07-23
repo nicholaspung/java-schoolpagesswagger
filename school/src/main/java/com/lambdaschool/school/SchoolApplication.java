bee409f73429a136ba92f666333bb45c61ec79d0
Nicholas Pung <nicholaspung>
2019-07-23 16:59:58 -0700

    Configured a bunch of files to get @EnableWebMvc to work with Swagger UI, but turns out theres no need for @EnableWebMvc if using Spring Framework

--- school/src/main/java/com/lambdaschool/school/SchoolApplication.java
+++ school/src/main/java/com/lambdaschool/school/SchoolApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+//@EnableWebMvc
 @EnableJpaAuditing
 @SpringBootApplication
 public class SchoolApplication
